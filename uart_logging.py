143e119faf7bc09759b17cecd88c9bb2b7a266ec
Austin Rosenbaum <austinjrosenbaum@gmail.com>
2018-02-14 13:37:51 -0700

    updated logging script.

--- uart_logging.py
+++ uart_logging.py
@@ -1,7 +1,17 @@
 import serial
 
-timeout = 60
+timeout = 0
+
+TILE_PKT_SIZE = 52
+HEALTH_PKT_SIZE = 111
 
 uart2 = serial.Serial(port = 'COM3', baudrate = 115200, timeout = timeout)
 
 log = open("pktlog.txt", 'wb')
+
+
+while True:
+    log.write(format(int(uart2.read(TILE_PKT_SIZE + 1)), '02X'))
+    log.write(format(int(uart2.read(HEALTH_PKT_SIZE + 1)), '02X'))
+
+log.close()
